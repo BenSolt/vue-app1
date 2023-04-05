978d0c3fb3dbb2c9e4cf4b24689396ba5653e246
benjamin-solt <bensolt@gmail.com>
2023-04-05 13:50:49 -0400

    trying to toggle todo item color

--- src/components/MyHelloWorld.vue
+++ src/components/MyHelloWorld.vue
@@ -5,11 +5,11 @@
         <button @click="subtract">Subtract</button>
         <h3>Number: {{ count }}</h3>
         <p>paragraph text goes here...</p>
-
+        <h3>Color: {{ color }}</h3>
         <h3 class="todoTitle">To Do List:</h3>
         <div class="todo-list">
             <ul class="todoBox">
-                <li v-for="x in todos" :key="x.id">
+                <li class="initial" :class="{ color }"  @click="changeColor" v-for="x in todos" :key="x.id">
                     {{ x.text }}
                 </li>
             </ul>
@@ -30,7 +30,8 @@ export default {
                 { text: 'learn Vue.js' },
                 { text: 'Build Something Awesome' }
             ],
-            count: 0
+            count: 0,
+            color: false
         };
     },
     methods: {
@@ -41,6 +42,9 @@ export default {
         subtract() {
             this.count--
         },
+        changeColor() {
+            this.color = !this.color
+        }
 
     },
 };
